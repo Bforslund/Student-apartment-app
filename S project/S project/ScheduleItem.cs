b798036bd5573b571621e97a501a315eb5a66a21
Polder,Colin C <c.polder@student.fontys.nl>
2019-12-11 12:31:04 +0100

    Uploaded the first version of the ScheduleItem class

---
+++ S project/S project/ScheduleItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_project
+{
+    class ScheduleItem
+    {
+        private DateTime dateTime;
+        private String name;
+        private bool done;
+        
+        public ScheduleItem()
+        {
+
+        }
+
+        public DateTime GetDateTime()
+        {
+            return this.dateTime;
+        }
+
+        public String GetName()
+        {
+            return this.name;
+        }
+
+        public bool GetDone()
+        {
+            return done;
+        }
+
+        public void SetDone(bool done)
+        {
+            this.done = done;
+        }
+
+
+    }
+}

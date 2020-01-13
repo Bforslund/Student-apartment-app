8d8395874b1173b781db86b466e4b100510d2888
Polder,Colin C <c.polder@student.fontys.nl>
2020-01-13 13:42:59 +0100

    Upload New File, the Arduino File for reading the temperature

---
+++ S project/ArduinoTemperatureReader.ino
@@ -0,0 +1,28 @@
+const int TempPort A1;
+const int NTC_R25 = 10000; // the resistance of the NTC at 25'C is 10k ohm
+const int NTC_MATERIAL_CONSTANT = 3950; // value provided by manufacturer
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(A1, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(get_temperature());
+  
+}
+
+
+float get_temperature()
+{
+  float temperature, resistance;
+  int value;
+  value = analogRead(NTC);
+  resistance   = (float)value * NTC_R25 / (1024 - value); // Calculate resistance
+  /* Calculate the temperature according to the following formula. */
+  temperature  = 1 / (log(resistance / NTC_R25) / NTC_MATERIAL_CONSTANT + 1 / 298.15) - 273.15;
+  return temperature;
+}
